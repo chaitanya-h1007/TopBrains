c9a2d47d19799f3c40e157fcae09e219f6a53dae
chaitanya harish <chaitanyaharish080@gmail.com>
2026-02-16 20:02:10 +0530

    Practice Problems

---
+++ Programming/Practice/Library.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+  public class Book
+  { 
+      public string ISBN { get; set; }
+      public string Title { get; set; }
+      public string Author { get; set; }
+      public string Genre { get; set; }
+      public bool IsAvailable { get; set; }
+      
+      
+      public static void Main(string[] args){
+        Catalog<Book> library = new Catalog<Book>();
+        
+        Book book1 = new Book 
+        { 
+            ISBN = "978-3-16-148410-0", 
+            Title = "C# Programming", 
+            Author = "John Sharp", 
+            Genre = "Programming" 
+        };
+        
+        library.AddItem(book1);
+        
+        var programmingBooks = library["Programming"];
+        Console.WriteLine(programmingBooks.Count); // Should output: 1
+        
+        var johnsBooks = library.FindBooks(b => b.Author.Contains("John"));
+        Console.WriteLine(johnsBooks.Count()); // Should output: 1
+
+      }
+  }
+  
+  // Generic catalog class
+  public class Catalog<T> where T : Book
+  {
+      private List<T> _items = new List<T>();
+      private HashSet<string> _isbnSet = new HashSet<string>();
+      private SortedDictionary<string, List<T>> _genreIndex = new SortedDictionary<string, List<T>>();
+      
+      // Add item with genre indexing
+      public void AddItem(T item)
+      {
+        
+          // TODO: Check ISBN uniqueness, add to list and genre index
+          if(_isbnSet.Contains(item.ISBN)){
+            throw new Exception("Cannot Add The Duplicate Values");
+          }
+          
+          _items.Add(item);
+          
+          
+          //this code maps the isbn with the list having same 
+          if (_genreIndex.ContainsKey(item.Genre))
+          {
+                _genreIndex[item.Genre].Add(item);
+                  
+          }
+          _genreIndex.Add(item.Genre, new List<T>());
+ 
+      }
+      
+      // Get books by genre using indexer
+      public List<T> this[string genre]
+      {
+          get
+          {
+              // TODO: Return books by genre or empty list
+              if (_genreIndex.ContainsKey(genre))
+                    return _genreIndex[genre];
+
+                return new List<T>();
+          }
+      }
+      
+      // Find books using LINQ and lambda expressions
+      public IEnumerable<T> FindBooks(Func<T, bool> predicate)
+      {
+          // TODO: Use LINQ Where with predicate
+      
+        
+          var res = _items
+            .Where(predicate);
+            
+            return res;
+      }
+      
+      
+      
+  }
+
+
+}
